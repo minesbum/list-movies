26e6352082c3326ab832d20dc57c6725da42579b
Adan <bonillaortezadan@gmail.com>
2023-09-12 20:23:55 -0600

    agregue los 2 hooks mas usados al codigo para que puedan alacenar informacion

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard"
 
 export const MovieList = (props) =>{
     const { list } = props;
+    const [favourites, setFavourites] = useState([]);
+
+    const handleFavouriteClick = (movieName) => {
+        console.log(`favourite clicker from ${movieName}`);
+
+        let newFavourites = [...favourites];
+    
+
+        if (!favourites.includes(movieName)){
+        newFavourites = [...newFavourites, movieName]
+         }else{
+        newFavourites = newFavourites.filter((movie) => movieName != movie);
+        }
+
+        setFavourites(newFavourites);
+
+
+        localStorage.setItem("favourites", JSON.stringify(newFavourites));
+    };
+
+    useEffect(() => {
+        const localStorageData = localStorage.getItem("favourites");
+        const storedFavourites = JSON.parse(localStorageData);
+
+        setFavourites(storedFavourites);
+    }, []);
+
+    console.log({ favourites });
+
+
     return(
         <ul>
             {list.map((movie) => (
-            <MovieCard name={movie.name} />
+            <MovieCard
+            key={movie.name}
+            name={movie.name}
+            onFavouriteClick={handleFavouriteClick}
+            isFavourite={favourites.includes(movie.name)}
+            />
             ))}
         </ul>
     )
